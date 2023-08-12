2b940e1c4f63c2261d912cc3d10f649f080bc84f
Mehmet BİLGİÇ <mbilgic2801@icloud.com>
2023-08-12 18:10:58 +0300

    deneme

--- assignments/week2/Question_02_01.java
+++ assignments/week2/Question_02_01.java
@@ -1,7 +1,16 @@
 package week2;
+
+import java.util.Scanner;
+
 public class Question_02_01{
 public static void main(String[]args){
+        double degreeCelsius;
+        double degreeFahrenheit;
+        Scanner input=new Scanner(System.in);
 
-        System.out.println("hello");
+        System.out.print("Enter a degree in Celsius: ");
+        degreeCelsius= input.nextDouble();
+        degreeFahrenheit=9.0/5*degreeCelsius+32;
+        System.out.println(degreeCelsius+" Celsius is "+degreeFahrenheit+" Fahrenheit.");
         }
         }
