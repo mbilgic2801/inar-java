c3f8f8fbb34ab4729fa10da0b2161ca3a6cdf2fc
Mehmet BİLGİÇ <mbilgic2801@icloud.com>
2023-11-16 04:55:13 +0300

    update

--- assignments/week11/Course.java
+++ assignments/week11/Course.java
@@ -1,65 +1,62 @@
 package week11;
 
+import java.util.ArrayList;
+
 public class Course {
     private String courseName;
-    private String[] students= new String[1];
-    private int numberOfStudents;
+    private ArrayList<String> students1=new ArrayList<>();
+   // private String[] students= new String[1];
+   // private int numberOfStudents;
     public Course(String courseName){
         this.courseName=courseName;
     }
     public void addStudent(String student){
-        enlargeSize();
-        students[numberOfStudents]=student;
-        numberOfStudents++;
-        System.out.println("After");
+      //  enlargeSize();
+      //  students[numberOfStudents]=student;
+      //  numberOfStudents++;
+      //  System.out.println("After");
+        students1.add(student);
+        System.out.println("After adding student to the course..");
+        System.out.println(this.toString());
+        System.out.println("Number of students: "+students1.size());
+
     }
-    public String[] getStudents(){
-        return students;
+    public ArrayList<String> getStudents(){
+        return students1;
     }
     public int getNumberOfStudents(){
-        return numberOfStudents;
+        return students1.size();
     }
     public String getCourseName(){
         return courseName;
     }
     public void dropStudent(String student){
-        if(studentExist(student)) {
-            String[] temp = new String[numberOfStudents - 1];
-            for (int i = 0, j = 0, k = 0; i < students.length; i++, j++) {
-                if (!students[i].equals(student)) {
-                    temp[k] = students[j];
-                    k++;
-                }
 
-            }
-            students = temp;
-            numberOfStudents--;
+        if(students1.contains(student)){
+            students1.remove(students1.indexOf(student));
+            System.out.println("After dropping student from the course..");
+            System.out.println(this.toString());
+            System.out.println("Number of students: "+students1.size());
         }
         else{
             System.out.println("The student you entered is not on this course");
         }
+
+    }
+
+    @Override
+    public String toString() {
+        String str="";
+        for (int i = 0; i < students1.size(); i++) {
+            str+=students1.get(i)+" - ";
+        }
+        return str;
     }
 
     private boolean studentExist(String student) {
 
-        for (int i = 0; i < students.length; i++) {
-            if(students[i].equals(student)){
-                return true;
-            }
-        }
-        return false;
+        return students1.contains(student);
     }
 
-    private void enlargeSize(){
-        if(numberOfStudents==students.length){
-            String[] temp=new String[numberOfStudents+1];
-            for (int i = 0; i < numberOfStudents; i++) {
-                temp[i]=students[i];
 
-            }
-            students=temp;
-
-        }
-
-    }
 }
--- assignments/week11/Question_11_05.java
+++ assignments/week11/Question_11_05.java
@@ -1,5 +1,19 @@
 package week11;
 
 public class Question_11_05 {
+    public static void main(String[] args) {
+        Course english=new Course("English");
+        english.addStudent("Nisanur Altuntas");
+        english.addStudent("Gurkan Serteser");
+        english.addStudent("Sinan Cetin");
+        english.addStudent("Mehmet Toprak");
+        english.addStudent("Sefa Atakul");
+        english.addStudent("Musa Denis");
+        english.addStudent("Berkan Eris");
+
+        english.dropStudent("Sinan Cetin");
+        english.dropStudent("Berkan Eris");
+        english.dropStudent("Mehmet Toprak");
+    }
 
 }
